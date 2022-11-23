e235cf912d67ff08f0e90deed519506971155a96
elizabeth-agada <elizadave6@gmail.com>
2022-11-24 00:32:55 +0100

    my fitness test project

---
+++ script.js
@@ -0,0 +1,55 @@
+const change = document.getElementById ("image1");
+
+change.addEventListener("click", (event) => {
+    event.target.style.background = "red";
+    event.target.style.color = "white";
+});
+
+const change2 = document.getElementById ("image2");
+
+change2.addEventListener("click", (event) => {
+    event.target.style.background = "darkgray";
+    event.target.style.color = "blue";
+});
+
+const change3 = document.getElementById ("image3");
+
+change3.addEventListener("click", (event) => {
+    event.target.style.background = "darkblue";
+    event.target.style.color = "white";
+});
+
+const change4 = document.getElementById ("image4");
+
+change4.addEventListener("click", (event) => {
+    event.target.style.background = "black";
+    event.target.style.color = "brown";
+});
+
+const change5 = document.getElementById ("image5");
+
+change5.addEventListener("click", (event) => {
+    event.target.style.background = "green";
+    event.target.style.color = "white";
+});
+
+const change6 = document.getElementById ("image6");
+
+change6.addEventListener("click", (event) => {
+    event.target.style.background = "orange";
+    event.target.style.color = "white";
+});
+
+const change7 = document.getElementById ("image7");
+
+change7.addEventListener("click", (event) => {
+    event.target.style.background = "skyblue";
+    event.target.style.color = "white";
+});
+
+const change8 = document.getElementById ("image8");
+
+change8.addEventListener("click", (event) => {
+    event.target.style.background = "crimson";
+    event.target.style.color = "white";
+});
